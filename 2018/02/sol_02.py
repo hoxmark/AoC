303113e4ea9d43b099c84c1a25a4cc6a5bc2ff1e
Bjørn Hoxmark <hoxmark@users.noreply.github.com>
2021-08-22 18:17:37 +0200

    update 2018 2 a

--- 2018/02/sol_02.py
+++ 2018/02/sol_02.py
@@ -7,16 +7,9 @@ with open("input.txt") as f:
 all_count = {2:0, 3:0}
 
 for l in lines:   
-    a = Counter(l)
-    print(a)
-    for c in list(set(a.values())):
-        if c in [2,3]:
-            all_count[c] += 1
+    counted_id = Counter(l)
+    for c in list(set(counted_id.values())):
+        if c in [2,3]: all_count[c] += 1
     
-print(all_count)
-
 sol_a = all_count[2]*all_count[3]
-
-#sol_a = functools.reduce(lambda a,b: a*b if a!=0 else b, all_count)
-
-print(sol_a)
+print(sol_a)    
