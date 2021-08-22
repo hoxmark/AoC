e7b65fdf9301215631d8f6e15e90e13fddbc581b
hoxmark <bhoxmark@gmail.com>
2021-08-22 18:06:17 +0000

    2018 2 b

--- 2018/02/sol_02_b.py
+++ 2018/02/sol_02_b.py
@@ -1,7 +1,25 @@
-from collections import Counter
-import functools
+import itertools
 
-with open("file2.txt") as f: 
+with open("input.txt") as f: 
     lines = [l.strip() for l in f.readlines()]
 
-print(lines)
+def findEqualWithOneError(s1,s2):
+    num_errors=0
+    index_error=-1
+    for i, (c1, c2) in enumerate(zip(s1, s2)):
+        if c1 != c2:
+            index_error = i
+            num_errors+=1
+        if num_errors>1: return -1
+    return index_error
+
+combs = itertools.combinations(lines,2)
+
+for c in combs:
+    res = findEqualWithOneError(*c)
+    if res>0:
+        print(c[0])
+        print(c[1])
+        print(c[0][:res]+c[0][res+1:])
+        break
+
