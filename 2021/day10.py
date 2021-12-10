83a5946646e2d5c1338fd6e6f548792306fd576d
hoxmark <bhoxmark@gmail.com>
2021-12-10 07:34:35 +0100

    refacor day 10

--- 2021/day10.py
+++ 2021/day10.py
@@ -2,77 +2,60 @@
 with open('data/input10.txt') as f:
     lines = [list(map(lambda x: x, l.strip())) for l in f]
 
-illegal_dict = {
-    ')': 3, 
-    ']': 57, 
-    '}': 1197,
-    '>': 25137
-}
+illegal_dict = {')': 3, ']': 57, '}': 1197, '>': 25137}
 
-pair = {
-    ')': '(',
-    ']': '[',
-    '}': '{',
-    '>': '<'
-}
+pair = {')': '(', ']': '[', '}': '{', '>': '<'}
 
-reversed_pair = {v:k for k,v in pair.items()}
+reversed_pair = {v: k for k, v in pair.items()}
 
-complete_comp = {
-    ')': 1,
-    ']': 2,
-    '}': 3,
-    '>': 4,
-}
-# %%
-def find_corruption(l):    
+complete_comp = {')': 1, ']': 2, '}': 3, '>': 4}
+
+
+# %% Task 1, find number of corrupt
+def find_corruption(l):
     stack = []
-    for i, c in enumerate(l): 
-        if c in ["[", '{', "(", "<"]:
-            stack.append(c)
-        else: 
-            left = stack.pop()                        
-            if left != pair[c]: 
-                print(left, c)
-                return illegal_dict[c]
+    for i, c in enumerate(l):
+        if c in ["[", '{', "(", "<"]: stack.append(c)
+        else:
+            left = stack.pop()
+            if left != pair[c]: return illegal_dict[c]
     return 0
-    
-sum([find_corruption(l) for l in lines])
+
+
+print("task1: ", sum([find_corruption(l) for l in lines]))
 
 # %% task2
-incomplete_lines = [l for l in lines if 0==find_corruption(l)]
+incomplete_lines = [l for l in lines if 0 == find_corruption(l)]
+
 
 def complete_lines(stack):
-    s = []
     score = 0
-    while len(stack)>0:
+    while len(stack) > 0:
         score *= 5
         c = stack.pop()
         r_c = reversed_pair[c]
         score += complete_comp[r_c]
     return score
 
+
+def traverse(l):
+    stack = []
+    for c in l:
+        if c in ["[", '{', "(", "<"]: stack.append(c)
+        else:
+            left = stack.pop()
+            if left != pair[c]: assert (False), "corruption"
+    return complete_lines(stack)
+
+
 def day10_2(lines):
-    res = []
-    for l in lines: 
-        stack = []
-        for i, c in enumerate(l): 
-            if c in ["[", '{', "(", "<"]:
-                stack.append(c)
-            else: 
-                left = stack.pop()                        
-                if left != pair[c]: 
-                    assert(False)
-        r = complete_lines(stack)                
-        res.append(r)        
-    
+    res = [traverse(l) for l in lines]
+
     #return middle of sorted res
-    return sorted(res)[int(len(res)/2)]
-    
-day10_2(incomplete_lines)
+    return sorted(res)[int(len(res) / 2)]
 
-# %%
 
-# %%
+print('Task 2:' ,day10_2(incomplete_lines))
+
 
 # %%
