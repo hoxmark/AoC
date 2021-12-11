80e32b8ae9e0c4cd32cc4704cafc8076da526fa2
hoxmark <bhoxmark@gmail.com>
2021-12-11 12:59:48 +0100

    2021 day 5

--- 2021/day05.py
+++ 2021/day05.py
@@ -1,5 +1,4 @@
 #%% Day 5
-
 with open('data/input5.txt') as f:
     lines = [
         list(
@@ -8,37 +7,87 @@ with open('data/input5.txt') as f:
     ]
 
 
+def make_grid(s):
+    gridline = []
+    for i in range(s):
+        gridline.append(0)
+
+    grid = []
+    for i in range(s):
+        grid.append(list(gridline))
+    return grid
+
+
 def flatten(t):
     return [item for sublist in t for item in sublist]
 
 
 all_points = flatten(flatten(lines))
-max_h, max_v = max(all_points), 0
+max_h, max_v = max(all_points) + 1, 0
 print(max_h, max_v)
 
-grid = [[0] * max_h] * max_h
+grid = make_grid(max_h)
 
 
 def hline(x, y, vertical):
     s = min([x, y])
-    e = max([x, y])
+    e = max([x, y]) + 1
     for i in range(s, e):
         grid[i][vertical] += 1
 
 
 def vline(x, y, horizontal):
     s = min([x, y])
-    e = max([x, y])
+    e = max([x, y]) + 1
     for i in range(s, e):
         grid[horizontal][i] += 1
 
 
-def add_line(x, y):
-    if x[0] == y[0]:
-        hline(x[1], y[1], x[0])
-    elif x[1] == y[1]:
-        vline(x[0], y[0], x[1])
+def dline(p1, p2):
+    #to right
+    if p1[0] < p2[0] and p1[1] < p2[1]:
+        current = p1
+        while current != p2:
+            grid[current[0]][current[1]] += 1
+            current = [current[0] + 1, current[1] + 1]
+        grid[p2[0]][p2[1]] += 1
+
+    elif p1[0] < p2[0] and p1[1] > p2[1]:
+        current = p1
+        while current != p2:
+            grid[current[0]][current[1]] += 1
+            current = [current[0] + 1, current[1] - 1]
+        grid[p2[0]][p2[1]] += 1
+    elif p1[0] > p2[0] and p1[1] < p2[1]:
+        current = p1
+        while current != p2:
+            grid[current[0]][current[1]] += 1
+            current = [current[0] - 1, current[1] + 1]
+        grid[p2[0]][p2[1]] += 1
+
+    elif p1[0] > p2[0] and p1[1] > p2[1]:
+        current = p1
+
+        while current != p2:
+            grid[current[0]][current[1]] += 1
+
+            current = [current[0] - 1, current[1] - 1]
+
+        grid[current[0]][current[1]] += 1
+    else:
+        assert (True), "Failed"
+
+def add_line(p1, p2):
+    if p1[0] == p2[0]:
+        vline(p1[1], p2[1], p1[0])
+    elif p1[1] == p2[1]:
+        hline(p1[0], p2[0], p1[1])
+    else:
+        dline(p1, p2)
 
 
 for l in lines:
     add_line(*l)
+
+print('task1', len([x for x in flatten(grid) if x > 1]))
+# %%
