d069da659a3434b1ab84c962944488aad52db895
hoxmark <bhoxmark@gmail.com>
2021-12-11 11:55:42 +0000

    forgot to add last part

--- 2021/day5_2.py
+++ 2021/day5_2.py
@@ -1,5 +1,5 @@
 
-with open('input5_test.txt') as f:
+with open('input5.txt') as f:
     lines = [list(map(lambda x: list(map(lambda x: int(x), tuple(x.split(',')))), tuple(l.strip().split(' -> ')))) for l in f]
 
 def make_grid(s): 
@@ -25,13 +25,14 @@ def hline(x,y, vertical):
     s = min([x,y])
     e = max([x,y])+1
     for i in range(s, e): 
-        grid[vertical][i] +=1
+        grid[i][vertical] +=1
+        #grid[vertical][i] +=1
 
 def vline(x,y,horizontal): 
     s = min([x,y])
     e = max([x,y])+1
     for i in range(s,e): 
-        grid[i][horizontal] +=1
+        grid[horizontal][i] +=1
 
 def dline(p1,p2):
     #print(p1, p2)
@@ -44,7 +45,7 @@ def dline(p1,p2):
         while current!=p2:
             grid[current[0]][current[1]] +=1
             current = [current[0]+1, current[1]+1]
-            print('loop', current)
+            #print('loop', current)
             #break
         grid[p2[0]][p2[1]] +=1
 
@@ -53,46 +54,49 @@ def dline(p1,p2):
         while current != p2:
             grid[current[0]][current[1]] +=1
             current = [current[0]+1, current[1]-1]
-            print('loop2', current)
-        grid[p2[0]][p2[1]] =1
-        print(p2)
-        print(grid)
+            #print('loop2', current)
+        grid[p2[0]][p2[1]] += 1
+        #print(p2)
     elif p1[0] > p2[0] and p1[1] <  p2[1]:
         current = p1
         while current != p2:
             grid[current[0]][current[1]] +=1
             current = [current[0]-1, current[1]+1]
-            print('loop3', current)
-        grid[p2[0]][p2[1]] +=1
+            #print('loop3', current)
+        grid[p2[0]][p2[1]] += 1
         
     elif p1[0] > p2[0] and p1[1] >  p2[1]:
         current = p1
+        #print('c',current)
         while current != p2:
             grid[current[0]][current[1]] +=1
+            #print('c',current)
             current = [current[0]-1, current[1]-1]
-            print('loop4', current)
-        grid[p2[0]][p2[1]] +=1
+            #print('loop4', current)
 
+        grid[current[0]][current[1]] += 1
     else: 
         print("What")
     
+    #for i in range(max_h):
+    #:    print(grid[i])
 
 
 def add_line(p1, p2): 
-    print(p1, p2)
+    #:print(p1, p2)
     if p1[0]==p2[0]: 
         vline(p1[1], p2[1], p1[0])
     elif p1[1] == p2[1]: 
         hline(p1[0], p2[0], p1[1])  
     else: 
         dline(p1,p2)
-
+#grid[2][0] = 11
 
 for l in lines: 
     add_line(*l)
 
-print('task1', len([x for x in flatten(grid) if x >1]))
-for i in range(max_h): 
-    print(i, grid[i])
+#print('task1', len([x for x in flatten(grid) if x >1]))
+#for i in range(max_h): 
+#   :    print( grid[i])
 
 print(len([x for x in flatten(grid) if x >1]))
