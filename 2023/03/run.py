3bc8b5c1fdcf81a32f6e00a63a36c01ab987376f
hoxmark <bjorn.hoxmark@finn.no>
2023-12-04 05:55:36 +0100

    tiny improvements

--- 2023/03/run.py
+++ 2023/03/run.py
@@ -1,6 +1,6 @@
 """Day two of advent of code 2023 https://adventofcode.com/2023 """
 # %% import data
-
+import math as m
 
 FILE_NAME = "input.txt"
 data = open(FILE_NAME, encoding="utf-8").read().splitlines()
@@ -11,26 +11,20 @@ flatten = lambda l: [item for sublist in l for item in sublist]
 def find_all_legal_adjacent(loc):
     i, j = loc
 
-    return [
-        (i, (j - 1)),  # left
-        (i, (j + 1)),  # right
-        ((i - 1), j),  # up
-        ((i + 1), j),  # down
-        ((i - 1), (j - 1)),  # up left
-        ((i - 1), (j + 1)),  # up right
-        ((i + 1), (j - 1)),  # down left
-        ((i + 1), (j + 1)),  # down right
-    ]
+    offsets = [(-1, -1), (-1, 0), (-1, 1), 
+               (0, -1),           (0, 1), 
+               (1, -1), (1, 0), (1, 1)]
+    
+    return [(i + di, j + dj) for di, dj in offsets]
 
 
 all_num = []
 all_num_loc = []
 symb_loc = {}
 for ir, r in enumerate(data):
-    r += "."
     num_loc = []
     num = []
-    for ii, i in enumerate(r):
+    for ii, i in enumerate(r + "."):  # ensure that we can find the last number
         if i.isdigit():
             num.append(i)
             num_loc.append((ir, ii))
@@ -72,5 +66,5 @@ for loc, symb in symb_loc.items():
                 nums.append(num)
 
         if len(nums) > 1:
-            task2 += nums[0] * nums[1]
+            task2 += m.prod(nums)
 assert task2 == 93994191
